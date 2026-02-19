919db2d86f8528f3e2aace3bab81fcb7647cd71e
Marek Horst <marek.horst@gmail.com>
2026-02-19 18:14:06 +0100

    Updating sample workflow to be compliant with the Airflow version 3.1.7

--- samples/simple-bash-workflow/sample-hello-world.py
+++ samples/simple-bash-workflow/sample-hello-world.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
-from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.bash  import BashOperator
 
 default_args = {
     'owner': 'airflow',
@@ -13,13 +13,12 @@ default_args = {
 }
 
 with DAG(
-    dag_id='simple_bash_hello_world',
-    default_args=default_args,
-    description='A simple bash workflow that prints Hello World',
-    schedule_interval=None,
-    start_date=datetime(2026, 1, 1),
-    catchup=False,
-    tags=['sample', 'bash'],
+        dag_id='simple_bash_hello_world',
+        default_args=default_args,
+        description='A simple bash workflow that prints Hello World',
+        start_date=datetime(2026, 1, 1),
+        catchup=False,
+        tags=['sample', 'bash'],
 ) as dag:
 
     hello_world_task = BashOperator(
