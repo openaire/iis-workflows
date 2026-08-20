bc9e29448cb4cedf3abf7bbbe5432313c538c5c9
Marek Horst <marek.horst@gmail.com>
2026-08-20 15:54:47 +0200

    Introducing an initial version of the DAG responsible for handling crossref mute citations.
    Updating `JAR_METADATAEXTRACTION` location to reflect jar with updated year extraction.

--- crossref/mute_citations_parsing_and_export.py
+++ crossref/mute_citations_parsing_and_export.py
@@ -33,7 +33,7 @@ default_args = {
     default_args=default_args,
     params={
         "JAR_METADATAEXTRACTION": Param(
-            default="https://maven.ceon.pl/artifactory/iis-snapshots/eu/dnetlib/iis/iis-wf-metadataextraction/1.3.0-SNAPSHOT/iis-wf-metadataextraction-1.3.0-20260819.103816-5-uber.jar",
+            default="https://maven.ceon.pl/artifactory/iis-snapshots/eu/dnetlib/iis/iis-wf-metadataextraction/1.3.0-SNAPSHOT/iis-wf-metadataextraction-1.3.0-20260820.134928-6-uber.jar",
             type='string',
             description="iis-wf-metadataextraction uber jar containing JsonReferenceParserJob"
         ),
